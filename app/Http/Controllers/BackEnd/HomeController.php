8e7a9af4712ddb1c885a05612b3b8ab5c571b430
nguyennhdn <nguyennhdn@gmail.com>
2021-04-30 09:00:29 +0700

    add kjk

---
+++ app/Http/Controllers/BackEnd/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\BackEnd;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+    }
+
+
+    public function index()
+    {
+        return view('home');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        return view('backend.index');
+    }
+}
